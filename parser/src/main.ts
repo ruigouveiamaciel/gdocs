0f62d376275bd1d34530608f8886725295d411b0
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-18 00:23:26 +0000

    added install and example scripts

--- parser/src/main.ts
+++ parser/src/main.ts
@@ -1,15 +1,20 @@
 import path from "path";
 import CategoryProject from "./classes/Project/CategoryProject";
 import fs from "fs";
+import parseArgs from "minimist";
 
 /**************************************************************** */
 
 const project = new CategoryProject();
+const args = parseArgs(process.argv.slice(2));
+const directories =
+	typeof args.d === "string" ? [args.d] : args.d instanceof Array ? args.d : [];
 
 /* Parse & Output */
-project.parse(path.join(__dirname, "..", "examples"));
-project.print();
+directories.forEach(directory => {
+	project.parse(path.join(process.cwd(), directory));
+});
 fs.writeFileSync(
-	path.join(__dirname, "..", "output.json"),
+	path.join(__dirname, "..", "..", "builder", "src", "parsed.json"),
 	JSON.stringify(project.structure, undefined, 4)
 );
