715f4e5128af34dbe4aada63689988cf9332dbd4
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-20 14:18:16 +0000

    or lists and fixed added spacing

--- builder/src/util/get_types.tsx
+++ builder/src/util/get_types.tsx
@@ -1,45 +1,54 @@
-import { category_types, project } from "./parsed"
+import { category_types, project } from "./parsed";
 import React from "react";
 import { Link } from "react-router-dom";
 
-function create_list(array: React.ReactElement[]): React.ReactElement {
+function create_list(
+	array: React.ReactElement[],
+	or_list: boolean = false
+): React.ReactElement {
 	if (array.length === 1) {
-		return <> {array[0]} </>;
+		return <>{array[0]}</>;
 	} else if (array.length > 1) {
-		return <>
-			{array.slice(0, -2).map(item => <>{item}{", "}</>)} {array[array.length - 2]} {" or "} {array[array.length - 1]}
-		</>;
+		return (
+			<>
+				{array.slice(0, -2).map((item) => (
+					<>
+						{item}
+						{or_list ? " or " : ", "}
+					</>
+				))}
+				{array[array.length - 2]} {" or "} {array[array.length - 1]}
+			</>
+		);
 	}
 
 	return <></>;
 }
 
-export default function get_types(string: string) {
-    const types = string.split("|")
-    const result: React.ReactElement[] = []
+export default function get_types(string: string, or_list: boolean = false) {
+	const types = string.split("|");
+	const result: React.ReactElement[] = [];
 
-    types.forEach((type) => {
-        let found = false;
+	types.forEach((type) => {
+		let found = false;
 
-        for (let i = 0; i < category_types.length; i++) {
-            const category = category_types[i]
+		for (let i = 0; i < category_types.length; i++) {
+			const category = category_types[i];
 
-            if (project[category].subcategories[type] !== undefined) {
-                result.push(
-                <Link to={`/${category}/${type}`}>{type}</Link>
-                )
+			if (project[category].subcategories[type] !== undefined) {
+				result.push(<Link to={`/${category}/${type}`}>{type}</Link>);
 
-                found = true;
-                break;
-            }
-        }
+				found = true;
+				break;
+			}
+		}
 
-        if (!found) {
-            result.push(
-            <a href={`https://wiki.facepunch.com/gmod/${type}`}>{type}</a>
-            )
-        }
-    })
+		if (!found) {
+			result.push(
+				<a href={`https://wiki.facepunch.com/gmod/${type}`}>{type}</a>
+			);
+		}
+	});
 
-    return create_list(result)
+	return create_list(result, or_list);
 }
