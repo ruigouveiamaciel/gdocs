19991d4ecfd2c1fffb6628824d313119f1104f26
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-20 19:41:24 +0000

    added realm message

--- builder/src/pages/TablePage/index.tsx
+++ builder/src/pages/TablePage/index.tsx
@@ -9,44 +9,56 @@ import get_types from "../../util/get_types";
 const TablePage: React.FC<{}> = () => {
 	const { tab, category, subcategory } = useParams();
 	const category_object = project[tab]?.subcategories?.[category];
-	const item =
-		(subcategory && "subcategories" in category_object
-			? category_object.subcategories[subcategory]
-			: category_object) as TablePageObject;
+	const item = (subcategory && "subcategories" in category_object
+		? category_object.subcategories[subcategory]
+		: category_object) as TablePageObject;
 	const description = item.description;
 	const fields = item.fields ?? [];
+	const realm = item.realm;
 
 	function markedDescription() {
 		return {
-			__html: marked(description as string),
+			__html: marked(
+				(description as string) +
+					(realm === "client" || realm === "server"
+						? `\n\nThis enum is only available on the **${realm}side**.`
+						: "")
+			),
 		};
 	}
 
 	return (
 		<Page title={item.name}>
-			{description && (<>
-				<Section>Description</Section>
-				<SectionContainer
-					dangerouslySetInnerHTML={markedDescription()}
-				/>
-			</>
+			{description && (
+				<>
+					<Section>Description</Section>
+					<SectionContainer
+						dangerouslySetInnerHTML={markedDescription()}
+					/>
+				</>
 			)}
-			{fields.length !== 0 && <>
-				<Section>Values</Section>
-				<SectionContainer>
-					<TableContainer>
-						<table>
-							<tbody>
-								{fields.map((field, index) => <tr key={`${index}-${field.key}--${field.type}`}>
-									<td>{get_types(field.type)}</td>
-									<td>{field.key}</td>
-									<td>{field.description}</td>
-								</tr>)}
-							</tbody>
-						</table>
-					</TableContainer>
-				</SectionContainer>
-			</>}
+			{fields.length !== 0 && (
+				<>
+					<Section>Values</Section>
+					<SectionContainer>
+						<TableContainer>
+							<table>
+								<tbody>
+									{fields.map((field, index) => (
+										<tr
+											key={`${index}-${field.key}--${field.type}`}
+										>
+											<td>{get_types(field.type)}</td>
+											<td>{field.key}</td>
+											<td>{field.description}</td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</TableContainer>
+					</SectionContainer>
+				</>
+			)}
 		</Page>
 	);
 };
