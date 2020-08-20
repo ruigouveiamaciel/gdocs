29f1fea2df2064a0f1054c4661ba5c3fd9cb1c10
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-20 20:40:33 +0000

    fixed runtime error when there was no language

--- builder/src/util/marked.ts
+++ builder/src/util/marked.ts
@@ -1,11 +1,15 @@
 import marked from "marked";
 import Prism from "prismjs";
 import "prismjs/components/prism-lua";
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism.css";
 
 marked.setOptions({
-	highlight: function (code, lang = "lua") {
-		return Prism.highlight(code, Prism.languages[lang], lang);
+	highlight: function (code, lang) {
+		if (Prism.languages[lang]) {
+			return Prism.highlight(code, Prism.languages[lang], lang);
+		} else {
+			return code;
+		}
 	},
 });
 
