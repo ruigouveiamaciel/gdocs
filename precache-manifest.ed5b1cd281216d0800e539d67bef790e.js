74b7f2686eb59d497c3304ced88e64ad20472cd8
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-21 02:23:51 +0000

    Updates

---
+++ precache-manifest.ed5b1cd281216d0800e539d67bef790e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f87c5cf881f0dfdb8f8509e496a16ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "468141b48d38061fdece",
+    "url": "./static/css/2.2b084824.chunk.css"
+  },
+  {
+    "revision": "468141b48d38061fdece",
+    "url": "./static/js/2.20dd1e6e.chunk.js"
+  },
+  {
+    "revision": "c90c8ae3244f49612816aa7cab780b18",
+    "url": "./static/js/2.20dd1e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb78ca73050e0c38b99",
+    "url": "./static/js/main.4b3f18ec.chunk.js"
+  },
+  {
+    "revision": "0896d2c0279d0d49f236",
+    "url": "./static/js/runtime-main.d63427c8.js"
+  }
+]);
